97c2c7e6ef81dcb7519dd7a422e3303d8dc671e2
Yassine El Haddad <megahaddad15@gmail.com>
2023-01-27 15:34:05 +0100

    latest updates, started working on parsing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:56:32 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/01/25 20:37:22 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/01/27 15:20:25 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,25 @@
 
 int main(int argc, char **argv)
 {
-	// t_stack stack_a;
+	t_stack	*stack_a;
 	// t_stack stack_b;
-	// size_t  i;
+	size_t	pos;
+
+	pos = 1;
 	if(argc == 1)
 		return(0);
 	argv ++;
 	if (has_errors(argv) || has_duplicates(argv, argc - 1) || is_sorted(argv, argc -1))
 		return (0);
 	printf("EVERYTHING OKAY !");
+	stack_a = ft_lstnew(argv ++, pos);
+	while (*argv)
+	{
+		argv ++;
+		pos += 1;
+	}
+	
+	// stack_a = 
 	// ft_atoi --- error --- dupl --- sorted
 	// while(av[])
 	// ft_lstnew
--- pushswap.h
+++ pushswap.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 17:08:37 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/01/25 20:26:12 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/01/27 15:18:45 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 
 typedef struct s_stack
 {
-	int data;
-	struct s_stack *next;
-	int position;
+	int	data;
+	struct s_stack	*next;
+	size_t	position;
 } t_stack;
 
 int	ft_isdigit(int c);
@@ -30,5 +30,6 @@ int	is_sorted(char **args, int count);
 int	ft_atoi(char *str);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(char *s);
+t_stack	*ft_lstnew(int content, int position);
 
 #endif
