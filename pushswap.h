89d5fe53619fd6a932ce44aac871978dbaf791f2
Yassine El Haddad <megahaddad15@gmail.com>
2023-02-03 23:38:51 +0100

    Update pushswap.h

--- pushswap.h
+++ pushswap.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 17:08:37 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/02/02 19:33:04 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/02/03 21:58:37 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 t_stack	*ft_lstnew(int content, size_t position);
 void	ft_lstadd_back(t_stack **lst, t_stack *new);
 void	add_index(t_stack **lst, t_stack **node);
+void	reverse_rotate(t_stack **a, char stack);
 int		has_duplicates(char **args, int count);
 int		is_sorted(char **args, int count);
 void	get_lst_position(t_stack **lst);
+void	swap(t_stack **a, char stack);
+t_stack *ft_before_last(t_stack *a);
 char	**ft_split(char *s, char c);
 void	ft_lstclear(t_stack **head);
 void	print_stack(t_stack **lst);
@@ -42,6 +45,5 @@ void 	free_2d(char **arr);
 size_t	ft_strlen(char *s);
 int		ft_atoi(char *str);
 int		ft_isdigit(int c);
-
 
 #endif
