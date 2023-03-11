42334c9d4a8418d06520b3dc38ed534c1181c289
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-11 23:19:30 +0100

    Create parse.c

---
+++ utils/parse.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/11 22:58:36 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/03/11 23:06:26 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pushswap.h"
+
+char	**parse(int argc, char **argv)
+{
+	int		i;
+	int		y;
+	int		z;
+	char	**tmp;
+	size_t	count;
+	char	**args;
+
+	i = -1;
+	count = 0;
+	while (++i < argc)
+		count += count_words(argv[i], ' ');
+	args = ft_calloc(count + 1, sizeof(char *));
+	i = -1;
+	y = -1;
+	while (++i < argc)
+	{
+		tmp = ft_split(argv[i], ' ');
+		z = 0;
+		while (tmp[z])
+			args[++y] = ft_strdup(tmp[z++]);
+		free_2d(tmp);
+	}
+	return (args);
+}
