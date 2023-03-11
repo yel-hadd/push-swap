7aee6ccd8e8817591e1f733b32fc7ad519852d78
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-11 23:20:12 +0100

    Update pushswap.h

--- pushswap.h
+++ pushswap.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 17:08:37 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/10 20:42:16 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/03/11 23:09:34 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,26 +38,33 @@ void	add_index(t_stack **lst, t_stack **node);
 int		ft_strncmp(char *s1, char *s2, size_t n);
 void	add_to_stack(t_stack **lst, char **argv);
 void	reverse_rotate(t_stack **a, char stack);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_calloc(size_t count, size_t size);
 void	sort_four(t_stack **a, t_stack **b);
 void	sort_five(t_stack **a, t_stack **b);
 void	get_lst_position(t_stack **lst);
 void	rotate(t_stack **a, char stack);
+char	**parse(int argc, char **argv);
 void	swap(t_stack **a, char stack);
 size_t	count_words(char *s, char c);
 t_stack *ft_before_last(t_stack *a);
 char	**ft_split(char *s, char c);
 void	ft_lstclear(t_stack **head);
 int		has_duplicates(char **args);
+void	ft_bzero(void *s, size_t n);
 void	print_stack(t_stack **lst);
 t_stack	*ft_lstlast(t_stack *lst);
 int		ft_lstsize(t_stack *lst);
 int		has_errors(char **args);
 void	sort_three(t_stack **a);
 int		is_sorted(char **args);
+int		empty_arg(char **argv);
 int		has_spaces(char *str);
+char	*ft_strdup(char *s1);
 void 	free_2d(char **arr);
 size_t	ft_strlen(char *s);
 int		ft_atoi(char *str);
 int		ft_isspace(char c);
 int		ft_isdigit(int c);
+
 #endif
