a01bd6839c347b893412214177202e133726aad1
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-11 23:20:16 +0100

    Create add_to_stack.c

---
+++ utils/add_to_stack.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_to_stack.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/11 22:48:34 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/03/11 23:03:37 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pushswap.h"
+
+void	add_to_stack(t_stack **lst, char **argv)
+{
+	t_stack	*node;
+
+	while (*argv)
+	{
+		node = ft_lstnew(ft_atoi(*argv), 0);
+		add_index(lst, &node);
+		ft_lstadd_back(lst, node);
+		argv ++;
+	}
+}
